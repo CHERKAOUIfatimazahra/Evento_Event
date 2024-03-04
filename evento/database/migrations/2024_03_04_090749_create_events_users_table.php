80b8e09fe9c811f85feccbdd17958bb91404fe9d
CHERKAOUIfatimazahra <cherkaoui.fatimazahra97@gmail.com>
2024-03-04 10:15:11 +0100

    create project laravel and add migrations and models

---
+++ evento/database/migrations/2024_03_04_090749_create_events_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->integer('quantity');
+            $table->string('place');
+            $table->string('status')->default('pending');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events_users');
+    }
+};
