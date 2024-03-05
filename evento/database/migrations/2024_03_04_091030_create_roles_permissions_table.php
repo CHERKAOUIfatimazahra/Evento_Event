1fda88e6ba2cd9de09812b1f2dd38a893b2722b3
CHERKAOUIfatimazahra <cherkaoui.fatimazahra97@gmail.com>
2024-03-05 10:01:13 +0100

    modify migration for roles and permissions

--- evento/database/migrations/2024_03_04_091030_create_roles_permissions_table.php
+++ evento/database/migrations/2024_03_04_091030_create_roles_permissions_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Permission;
+use App\Models\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,11 +13,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('permission_role', function (Blueprint $table) {
+        Schema::create('roles_permissions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('permission_id')->constrained();
-            $table->foreignId('role_id')->constrained();
-            $table->timestamps();
+            $table->foreignIdFor(Role::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Permission::class)->constrained()->cascadeOnDelete();
         });
     }
 
--- evento/database/migrations/2024_03_05_085325_create_users_permissions_table.php
+++ evento/database/migrations/2024_03_05_085325_create_users_permissions_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Permission;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,11 +13,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('role_user', function (Blueprint $table) {
+        Schema::create('users_permissions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('role_id')->constrained();
-            $table->foreignId('user_id')->constrained();
-            $table->timestamps();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Permission::class)->constrained()->cascadeOnDelete();
         });
     }
 
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('role_user');
+        Schema::dropIfExists('users_permissions');
     }
 };
