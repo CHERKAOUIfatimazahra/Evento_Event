f449c910d1fa3b7063f9ee16a72cd541d33ce8b5
CHERKAOUIfatimazahra <cherkaoui.fatimazahra97@gmail.com>
2024-03-05 14:55:07 +0100

    form validation for event

---
+++ evento/app/Http/Requests/UpdateEventRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateEventRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'location' => 'required|string|max:255',
+            'start_datetime' => 'required|date',
+            'end_datetime' => 'required|date|after:start_datetime',
+            'type' => 'required|in:Physical,Online',
+            'price' => 'required|numeric|min:0',
+            'tickets_available' => 'required|integer|min:0',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif',
+        ];
+    }
+}
