7772e9028224e7f411a850dd0508e6406b7029ea
CHERKAOUIfatimazahra <cherkaoui.fatimazahra97@gmail.com>
2024-03-06 22:22:19 +0100

    create function statistique

--- evento/app/Http/Controllers/StaticController.php
+++ evento/app/Http/Controllers/StaticController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Event;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class StaticController extends Controller
 {
     public function statisTotal()
     {
+        $users_count = User::count();
+        $events_count = Event::count();
+        $categories_count = Category::count();
+        $roles_count = Role::count();
+        
         return view('dashbord.statistique');
     }
 
